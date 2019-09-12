9459c799be9ae7cbc17ff66b44124a4bd17107fe
tuanminh160302 <canhtuan09@gmail.com>
2019-09-13 01:08:45 +0700

    13.9.2019

---
+++ js/fScr.js
@@ -0,0 +1,9 @@
+/* Open */
+function openNav() {
+    document.getElementById("myNav").style.height = "100vh";
+  }
+  
+  /* Close */
+  function closeNav() {
+    document.getElementById("myNav").style.height = "0vh";
+  }
