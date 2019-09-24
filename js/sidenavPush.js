819fae0ff5feb5e65dbd19c9292efcd470d98c4a
tuanminh160302 <canhtuan09@gmail.com>
2019-09-24 20:55:30 +0700

    24.9.2019 9PM

---
+++ js/sidenavPush.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("container-demo-1").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("container-demo-1").style.marginLeft= "0";
+  }
