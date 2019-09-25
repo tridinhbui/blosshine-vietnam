b4b363c0d3775612863b9f31990284a6ae7174c2
tuanminh160302 <canhtuan09@gmail.com>
2019-09-25 22:19:21 +0700

    25.9.2019

--- js/sidenavPush1.js
+++ js/sidenavPush1.js
@@ -1,13 +1,13 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("container-demo-1").style.marginLeft = "250px";
+    document.getElementById("container").style.marginLeft = "250px";
     document.getElementById("closeBtn").style.zIndex = "10";
     document.getElementById("openBtn").style.zIndex = "-1";
   }
   
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("container-demo-1").style.marginLeft= "0";
+    document.getElementById("container").style.marginLeft= "0";
     document.getElementById("closeBtn").style.zIndex = "-1";
     document.getElementById("openBtn").style.zIndex = "10";
   }
