469a8d56b5a8a1a5c1346ca24cab977482b90de9
tuanminh160302 <canhtuan09@gmail.com>
2019-09-29 17:59:50 +0700

    29.9.2019 6PM

--- js/stickyScroll.js
+++ js/stickyScroll.js
@@ -4,15 +4,6 @@ let mainSections = document.querySelectorAll("main section");
 let lastId;
 let cur = [];
 
-// This should probably be throttled.
-// Especially because it triggers during smooth scrolling.
-// https://lodash.com/docs/4.17.10#throttle
-// You could do like...
-// window.addEventListener("scroll", () => {
-//    _.throttle(doThatStuff, 100);
-// });
-// Only not doing it here to keep this Pen dependency-free.
-
 window.addEventListener("scroll", event => {
   let fromTop = window.scrollY;
 
