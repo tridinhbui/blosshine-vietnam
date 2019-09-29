f153dd9a3c7805f37f0bbb4f6d5e8a72872bb16f
tuanminh160302 <canhtuan09@gmail.com>
2019-09-29 07:36:19 +0700

    29.9.2019

--- js/sidenavPush-stories.js
+++ js/sidenavPush-stories.js
@@ -1,13 +1,13 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "20%";
-    document.getElementById("container-demo-1").style.marginLeft = "20%";
+function openNavStories() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("container").style.marginLeft = "250px";
     document.getElementById("closeBtn").style.visibility = "visible";
     document.getElementById("openBtn").style.visibility = "hidden";
   }
   
-  function closeNav() {
+  function closeNavStories() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("container-demo-1").style.marginLeft= "0";
+    document.getElementById("container").style.marginLeft= "0";
     document.getElementById("closeBtn").style.visibility = "hidden";
     document.getElementById("openBtn").style.visibility = "visible";
   }
